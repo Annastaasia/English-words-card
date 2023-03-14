ad215d3e2b169e2744718e2d93411a4bb02cd593
Anastasiya3895641 <anastasiya-shitenkova@mail.ru>
2023-03-14 23:52:53 +0300

    add logo and sticky header

--- englishapp/src/Components/Header.jsx
+++ englishapp/src/Components/Header.jsx
@@ -1,10 +1,15 @@
+import mainLogo from "../assets/image/logo.png";
+
 function Header() {
   return (
     <header>
       <div className="App__header">
+        <img src={mainLogo} alt="logo" className="App__header__logo" />
         <h1 className="App__header__h1">English cards</h1>
-
-        <button className="App__header__game"></button>
+        <div className="App__header__buttons">
+          <button className="App__header__game"></button>
+          <button className="App__header__home"></button>
+        </div>
       </div>
     </header>
   );
